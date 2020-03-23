920605e267a5c48f98b152a3f36239d20576f89a
lijinbao <651738941@qq.com>
2020-03-23 22:49:07 +0800

    创建控制器

---
+++ web/controller/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * File: index.php
+ * Functionality:
+ * Author: Li
+ * Date: 2020/3/23
+ */
+namespace web\controller;
+class Index{
+    public  function show(){
+        echo 'this is a coposer study!!';
+    }
+}
