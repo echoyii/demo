4a6460becd7113057ed7522e0d63cd50447796a9
lijinbao <651738941@qq.com>
2020-03-28 21:36:18 +0800

    echo mini框架 搭建

--- core/Bootstrap.php
+++ core/Bootstrap.php
@@ -11,7 +11,6 @@ namespace core;
 use web\controller;
 class Bootstrap {
     public static function run(){
-//        echo 33;
         self::parseUrl();
     }
     //分析url分发路由到控制器方法
@@ -19,16 +18,18 @@ class Bootstrap {
 //        dd($_SERVER);
         if (isset($_GET['s'])){
             $s_arr = explode('/',$_GET['s']);
-            dd($s_arr);
+//            dd($s_arr);
             $class = "\\web\\controller\\".ucfirst($s_arr[0]);
             $action = $s_arr[1];
-            (new $class())->$action();
+
         } else {
 
             $class = "\\web\\controller\\Index";
             $action = "show";
-            (new $class())->$action();
+
         }
+        //当一个对象被当作字符串对待的时候，会触发这个魔术方法__toString()
+        echo (new $class())->$action();
     }
 
 }
--- web/controller/index.php
+++ web/controller/index.php
@@ -6,8 +6,20 @@
  * Date: 2020/3/23
  */
 namespace web\controller;
+use core\View;
+
 class Index{
-    public  function show(){
-        echo 'this is a coposer study!!';
+    protected $view;
+    public function __construct()
+    {
+        $this->view = new View();
     }
+
+
+
+    public  function show(){
+//        echo 'this is a coposer study!!';
+        return $this->view->make('index')->with('version','版本：1.0 copyright: echo LiLi');
+    }
+
 }
